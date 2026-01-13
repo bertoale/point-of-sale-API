c9b0d261d5694e75f2ea7646285318f8ae6e5b66
bertoale <albertdarma@gmail.com>
2026-01-13 22:34:36 +0800

    refactor response messages in product controller for consistency

--- controllers/product.js
+++ controllers/product.js
@@ -4,7 +4,7 @@ import { Success, Error } from "../utils/response.js";
 
 export const GetAllProducts = asyncHandler(async (req, res) => {
   const products = await Product.findAll();
-  return Success(res, 200, { products });
+  return Success(res, 200, "Products retrieved successfully", { products });
 });
 
 export const GetProductById = asyncHandler(async (req, res) => {
@@ -13,36 +13,36 @@ export const GetProductById = asyncHandler(async (req, res) => {
   if (!product) {
     return Error(res, 404, "Product not found");
   }
-  return Success(res, 200, { product });
+  return Success(res, 200, "Product retrieved successfully", { product });
 });
 
 export const CreateProduct = asyncHandler(async (req, res) => {
-  const { CategoryId, name, sellingPrice, purchasePrice, stock } = req.body;
+  const { categoryId, name, sellingPrice, purchasePrice, stock } = req.body;
   const newProduct = await Product.create({
-    CategoryId,
+    categoryId,
     name,
     sellingPrice,
     purchasePrice,
     stock,
   });
-  return Success(res, 201, { product: newProduct });
+  return Success(res, 201, "Product created successfully", newProduct);
 });
 
 export const UpdateProduct = asyncHandler(async (req, res) => {
   const productId = req.params.id;
-  const { CategoryId, name, sellingPrice, purchasePrice, stock } = req.body;
+  const { categoryId, name, sellingPrice, purchasePrice, stock } = req.body;
   const product = await Product.findByPk(productId);
   if (!product) {
     return Error(res, 404, "Product not found");
   }
   await product.update({
-    CategoryId,
+    categoryId,
     name,
     sellingPrice,
     purchasePrice,
     stock,
   });
-  return Success(res, 200, { product });
+  return Success(res, 200, "Product updated successfully", product);
 });
 
 export const DeleteProduct = asyncHandler(async (req, res) => {
